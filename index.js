7d2eb4628162780f41c528571f78727b57a20804
JonathanHill513 <91477154+JonathanHill513@users.noreply.github.com>
2021-11-19 09:26:12 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+var numPets = document.getElementsByClassName("petnum").length;
+console.log(numPets)
+
+document.getElementById("petcount")
+function count(numPets){
+  document.getElementById("petcount")=petcount
+}
+
+// document.write(countsentence)
+
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
---
+++ petstore.js
@@ -0,0 +1,124 @@
+var shop = [
+ 
+    {
+        title: 'Benny',
+        image: './imgs/jamie-street-MoDcnVRN5JU-unsplash.jpg',
+        price: '$70',
+        species: 'Dog',
+        description: 'Loves: Bones <br> Hates: Mail men',
+        location: 'Westwood'
+    },
+    {
+        title: 'Frank',
+        image: './imgs/craig-pattenaude-o5LcQ9GkT6A-unsplash.jpg',
+        price: '$100',
+        species: 'Turtle',
+        description: 'Loves: Leafs <br> Hates: Plastic',
+        location: 'West Chester'
+    },
+    {  title: 'James',
+        image: './imgs/james-wainscoat-yEW23jxVsNI-unsplash.jpg',
+        price: '$25',
+        species: 'Chicken',
+        description: 'Loves: Seeds <br> Hates: KFC',
+        location: 'Western Hills'
+    },
+    {
+        title: 'Benny',
+        image: './imgs/jamie-street-MoDcnVRN5JU-unsplash.jpg',
+        price: '$70',
+        species: 'Dog',
+        description: 'Loves: Bones <br> Hates: Mail men',
+        location: 'Westwood'
+    },
+    {
+        title: 'Frank',
+        image: './imgs/craig-pattenaude-o5LcQ9GkT6A-unsplash.jpg',
+        price: '$100',
+        species: 'Turtle',
+        description: 'Loves: Leafs <br> Hates: Plastic',
+        location: 'West Chester'
+    },
+    {  title: 'James',
+        image: './imgs/james-wainscoat-yEW23jxVsNI-unsplash.jpg',
+        price: '$25',
+        species: 'Chicken',
+        description: 'Loves: Seeds <br> Hates: KFC',
+        location: 'Western Hills'
+    },
+    {
+        title: 'Benny',
+        image: './imgs/jamie-street-MoDcnVRN5JU-unsplash.jpg',
+        price: '$70',
+        species: 'Dog',
+        description: 'Loves: Bones <br> Hates: Mail men',
+        location: 'Westwood'
+    },
+    {
+        title: 'Frank',
+        image: './imgs/craig-pattenaude-o5LcQ9GkT6A-unsplash.jpg',
+        price: '$100',
+        species: 'Turtle',
+        description: 'Loves: Leafs <br> Hates: Plastic',
+        location: 'West Chester'
+    },
+    {  title: 'James',
+        image: './imgs/james-wainscoat-yEW23jxVsNI-unsplash.jpg',
+        price: '$25',
+        species: 'Chicken',
+        description: 'Loves: Seeds <br> Hates: KFC',
+        location: 'Western Hills'
+    },
+    {
+        title: 'Benny',
+        image: './imgs/jamie-street-MoDcnVRN5JU-unsplash.jpg',
+        price: '$70',
+        species: 'Dog',
+        description: 'Loves: Bones <br> Hates: Mail men',
+        location: 'Westwood'
+    },
+    {
+        title: 'Frank',
+        image: './imgs/craig-pattenaude-o5LcQ9GkT6A-unsplash.jpg',
+        price: '$100',
+        species: 'Turtle',
+        description: 'Loves: Leafs <br> Hates: Plastic',
+        location: 'West Chester'
+    },
+    {  title: 'James',
+        image: './imgs/james-wainscoat-yEW23jxVsNI-unsplash.jpg',
+        price: '$25',
+        species: 'Chicken',
+        description: 'Loves: Seeds <br> Hates: KFC',
+        location: 'Western Hills'
+    },
+
+
+
+
+
+
+
+
+    
+    ]
+
+
+    var postHTML = " "
+  
+    for (var i=0; i < shop.length; i++){
+        var heading = '<div class="product col-lg-2 my-1 col-md-3 col-sm-6 col-xs-12 petnum' + '"><span><h5>' + shop[i].title + '</h5></span>'
+        var image = '<img src="' + shop[i].image + '"/'
+        var price = '<p><div class="price">' + shop[i].price + '</div></p>'
+        var description = '<p class="species">Species: '+shop[i].species+'</p><p>'+shop[i].description+"</p><p> Location: "+shop[i].location + '</p><a href="./purchase.html"><button type="button" class="btn btn-warning">Buy</button></div></a>'
+        var concatThis = heading + image + price + description;
+        postHTML = postHTML + concatThis
+        
+    }
+    document.getElementById('market').innerHTML = postHTML
+    
+      var petcount = document.getElementsByClassName("petnum").length
+    
+    document.getElementById('petcount').innerHTML = "<h2>We have " +petcount+ " reasons for you to shop with us!</h2>"
+
+    
